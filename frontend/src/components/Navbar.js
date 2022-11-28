6a81b9c29ac206c9a992cb604009578a35b0b544
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2022-11-28 09:34:00 +0700

    done navbar components

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-// import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Badge from "@mui/material/Badge";
 
 const Navbar = () => {
+  const style = "text-[14px] cursor-pointer ml-[25px]";
+
   return (
     <div className="navbar h-[60px] shadow-md relative z-10">
       <div className="wrapper pl-[20px] pr-[20px] pt-[10px] pb-[10px] flex justify-between items-center">
@@ -16,14 +18,21 @@ const Navbar = () => {
           </div>
         </div>
 
-        {/* Logo div */}
+        {/* Logo */}
         <div className="center flex-1 text-center">
           <div className="logo font-bold text-lg">LOGO IMAGE</div>
         </div>
 
-        {/* 25:31 */}
-
-        <div>3</div>
+        {/* Right */}
+        <div className="right flex flex-1 items-center justify-end">
+          <div className={style}>Register</div>
+          <div className={style}>Sign in</div>
+          <div className={style}>
+            <Badge badgeContent={2} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </div>
+        </div>
       </div>
     </div>
   );
